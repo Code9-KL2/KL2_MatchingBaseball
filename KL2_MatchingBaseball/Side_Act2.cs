3c0dd325ca556882295941903512d9e537705437
lt051210 <lt051210@naver.com>
2018-09-05 21:30:27 +0900

    edit everything

--- KL2_MatchingBaseball/Side_Act2.cs
+++ KL2_MatchingBaseball/Side_Act2.cs
@@ -17,102 +17,121 @@ namespace KL2_MatchingBaseball
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            base.OnCreate(savedInstanceState);
+            int[] m_data;
+            Side_Act m_Player;
+            void NumberBaseBall()
+            {
+                m_data = new int[3];
+                m_Player = new Side_Act();
+            }
+            void makeData()
+            {
+                Random rand;
+                rand = new Random();
 
-            // Create your application here
-            SetContentView(Resource.Layout.Side_Layout2);
+                for (int i = 0; i < m_data.Length; i++)
+                {
+                    m_data[i] = rand.Next(0, 9);
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (m_data[i] == m_data[j])
+                        {
+                            i--;
+                        }
+                    }
 
-            Random random = new Random();
-            int n = random.Next(0,  9);
-            int n1 = random.Next(0, 9);
-            int n2 = random.Next(0, 9);
-            string number = Convert.ToString(n);
-            string number1 = Convert.ToString(n1);
-            string number2 = Convert.ToString(n2);
+                }
+            }
+            void showMsg(String msg)
+            {
+                Toast.MakeText(this, msg,ToastLength.Long).Show();
+            }
+            void showPlay(int[] data)
+            {
+                Toast.MakeText(this, "스트라이크:" + data[1] + " ,볼:" + data[0],ToastLength.Long).Show();
+            }
+            void showGameEnd(Boolean win)
+            {
+                if (true == win)
+                {
+                    Toast.MakeText(this, "축하합니다", ToastLength.Long).Show();
+                }
+                Toast.MakeText(this, "컴퓨터의 숫자는?", ToastLength.Long).Show();
+                for (int i = 0; i < m_data.Length; i++)
+                {
+                    Toast.MakeText(this, m_data[i], ToastLength.Long).Show();
+                }
+            }
+            int check(int index, int a)
+            {
+                int b;
 
-            string main_number = number + number1 + number2;
+                b = 0;
+                for (int i = 0; i < m_data.Length; i++)
+                {
+                    if (m_data[i] == a)
+                    {
+                        if (i == index)
+                        {
+                            b = 2;
+                            break;
+                        }
+                        else
+                        {
+                            b = 1;
+                        }
+                    }
+                }
+                return b;
+            }
+            void onePlay(int[] result)
+            {
+                int input;
+                int r;
+                for (int i = 0; i < result.Length; i++)
+                {
+                    result[i] = 0;
+                }
+                for (int i = 0; i < 3; i++)
+                {
+                    input = m_Player.input();
+                    r = check(i, input);
 
-            Button btn0 = FindViewById<Button>(Resource.Id.btn0);
-            Button btn1 = FindViewById<Button>(Resource.Id.btn1);
-            Button btn2 = FindViewById<Button>(Resource.Id.btn2);
-            Button btn3 = FindViewById<Button>(Resource.Id.btn3);
-            Button btn4 = FindViewById<Button>(Resource.Id.btn4);
-            Button btn5 = FindViewById<Button>(Resource.Id.btn5);
-            Button btn6 = FindViewById<Button>(Resource.Id.btn6);
-            Button btn7 = FindViewById<Button>(Resource.Id.btn7);
-            Button btn8 = FindViewById<Button>(Resource.Id.btn8);
-            Button btn9 = FindViewById<Button>(Resource.Id.btn9);
+                    if (2 == r)
+                    {
+                        result[1]++;
+                    }
+                    else if (1 == r)
+                    {
+                        result[0]++;
+                    }
+                }
+            }
+            void run()
+            {
+                Boolean win;
+                int[] result;
 
-            btn0.Click += btn0_Click;
-            btn1.Click += btn1_Click;
-            btn2.Click += btn2_Click;
-            btn3.Click += btn3_Click;
-            btn4.Click += btn4_Click;
-            btn5.Click += btn5_Click;
-            btn6.Click += btn6_Click;
-            btn7.Click += btn7_Click;
-            btn8.Click += btn8_Click;
-            btn9.Click += btn9_Click;
+                result = new int[2];
+                makeData();
 
+                win = false;
+                for (int i = 0; i < 10; i++)
+                {
+                    showMsg((i + 1) + "번째 시도");
 
+                    onePlay(result);
+                    showPlay(result);
+                }
+                showGameEnd(win);
+            }
+            static void Main(string[] args)
+            {
+                Program m = new Program();
+                m.NumberBaseBall();
+                m.run();
 
-        }
-
-        int Count = 0;
-
-        int User_Input;
-        int Number_F;
-        int Number_M;
-        int Number_L;
-
-        private void btn9_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btn8_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btn7_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btn6_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btn5_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btn4_Click(object sender, EventArgs e)
-        {
- 
-        }
-
-        private void btn3_Click(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void btn2_Click(object sender, EventArgs e)
-        {   
-            
-        }
-
-        private void btn1_Click(object sender, EventArgs e)
-        {
- 
-        }
-
-        private void btn0_Click(object sender, EventArgs e)
-        {
-
+            }
         }
     }
 }
